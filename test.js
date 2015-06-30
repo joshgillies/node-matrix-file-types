365cc60cb63135c0bcd822d45d3b041c9199605f
Josh Gillies <github@joshgilli.es>
2015-06-30 16:04:42 +1000

    Add test messages

--- test.js
+++ test.js
@@ -2,8 +2,8 @@ var test = require('tape')
 var fileAsset = require('./')
 
 test('basic usage', function (assert) {
-  assert.equal(fileAsset('image.jpeg'), 'image')
-  assert.equal(fileAsset('file.txt'), 'text_file')
-  assert.equal(fileAsset('file.dunno'), 'file')
+  assert.equal(fileAsset('image.jpeg'), 'image', 'test .jpeg')
+  assert.equal(fileAsset('file.txt'), 'text_file', 'test .txt')
+  assert.equal(fileAsset('file.dunno'), 'file', 'default to file for unknown file types')
   assert.end()
 })
