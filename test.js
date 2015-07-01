b6fe43b9bf2767ab87c57258a14d7b0bb5745758
Josh Gillies <github@joshgilli.es>
2015-07-01 23:20:16 +1000

    Add test for .js files

--- test.js
+++ test.js
@@ -4,6 +4,7 @@ var fileAsset = require('./')
 test('basic usage', function (assert) {
   assert.equal(fileAsset('image.jpeg'), 'image', 'test .jpeg')
   assert.equal(fileAsset('file.txt'), 'text_file', 'test .txt')
+  assert.equal(fileAsset('script.js'), 'file', 'test .js')
   assert.equal(fileAsset('styles.css'), 'css_file', 'test .css')
   assert.equal(fileAsset('file.dunno'), 'file', 'default to file for unknown file types')
   assert.equal(fileAsset('file.json'), 'file', 'don\'t match js in json')
